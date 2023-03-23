d31cd843981e17f103ba26b6422113c380a02de8
AustinWildgrube <austinwildgrube@gmail.com>
2023-03-22 19:23:09 -0500

    feat: rotate the accordion chevron when clicked

--- components/figure.tsx
+++ components/figure.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -9,7 +9,7 @@ import { ChevronDown, ExternalLink, Figma, GitHub } from 'react-feather';
 
 import { AccordionContent, AccordionItem, AccordionTrigger } from '&/components/ui/accordion';
 import { Badge } from '&/components/ui/badge';
-import { cardVariants } from '&/lib/utils';
+import { cardVariants, cn } from '&/lib/utils';
 
 interface FigureProps {
   title: string;
@@ -38,6 +38,8 @@ export function Figure({
   githubLink,
   externalLink,
 }: FigureProps): JSX.Element {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
   return (
     <motion.div
       className="border-b border-b-gray-200 py-8 last:border-b-0"
@@ -46,10 +48,20 @@ export function Figure({
       viewport={{ once: true, amount: 0.3 }}>
       <motion.div variants={cardVariants}>
         <AccordionItem value={title}>
-          <AccordionTrigger className="flex flex-col gap-x-16 lg:flex-row lg:items-center lg:justify-between">
+          <AccordionTrigger
+            className="flex flex-col gap-x-16 lg:flex-row lg:items-center lg:justify-between"
+            onClick={() => setIsExpanded(!isExpanded)}>
             <div className="mb-8 max-w-xl lg:mb-0">
               <h2 className="mb-3 text-2xl font-semibold">
-                {title} <ChevronDown className="inline-block" size={21} />
+                {title}{' '}
+                <ChevronDown
+                  className={cn(
+                    'ease-[cubic-bezier(0.87,_0,_0.13,_1)] inline-block transition-transform duration-300',
+                    isExpanded && 'rotate-180',
+                  )}
+                  size={21}
+                  aria-hidden
+                />
               </h2>
 
               <p className="mb-6">{description}</p>
