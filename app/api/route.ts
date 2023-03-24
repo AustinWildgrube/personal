be9ca0d508168b2435793cdc19a270aba53f4429
AustinWildgrube <austinwildgrube@gmail.com>
2023-03-24 10:09:17 -0500

    fix: set revalidation to 0
    There seems to be a bug when the revalidation variable is set to anything other than 0. I'll increase it when this bug is fixed.

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 
 import { currentlyPlayingSong } from '&/lib/spotify';
 
+export const revalidate = 0;
+
 export async function GET(request: Request) {
   const response = await currentlyPlayingSong();
 
